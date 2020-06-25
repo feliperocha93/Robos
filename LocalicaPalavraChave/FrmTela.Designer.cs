24eabcbc032ac5699c274f3e8544d454ff82b6e6
jessica cruz <jessicacsilva20@gmail.com>
2020-06-25 13:51:34 -0300

    Add projeto

---
+++ LocalicaPalavraChave/FrmTela.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LocalicaPalavraChave
+{
+    partial class FrmTela
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Caminho que será pesquisado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Palavra Chave";
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Location = new System.Drawing.Point(28, 49);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(497, 20);
+            this.txtCaminho.TabIndex = 2;
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(28, 109);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(497, 20);
+            this.txtFiltro.TabIndex = 3;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(28, 148);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(158, 23);
+            this.btnPesquisar.TabIndex = 4;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.White;
+            this.lblResultado.Location = new System.Drawing.Point(28, 206);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 6;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(197, 148);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(158, 23);
+            this.btnClean.TabIndex = 7;
+            this.btnClean.Text = "Limpar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.bntLimpar_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(367, 148);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(158, 23);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "Copiar";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 604);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.txtCaminho);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Localiza Arquivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCaminho;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ LocalicaPalavraChave/FrmTela.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LocalicaPalavraChave
+{
+    public partial class FrmTela : Form
+    {
+        public FrmTela()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Evento dos Botões
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            DesabilitarBotoes();
+            var caminho = txtCaminho.Text;
+            var filtro = !string.IsNullOrEmpty(txtFiltro.Text) ? txtFiltro.Text.Split(';') : new string[1];
+            var listaResultado = new List<string>();
+            var arquivos = Directory.GetFiles(@caminho, "*.*", SearchOption.AllDirectories)
+                .Where(s => !s.EndsWith(".dll")
+                && !s.EndsWith(".csproj")
+                && !s.EndsWith(".git")
+                && !s.EndsWith(".snk")
+                && !s.EndsWith(".png")
+                && !s.EndsWith(".git")
+                && !s.EndsWith(".gif")
+                && !s.EndsWith(".jpg")
+                && !s.EndsWith(".css")
+                && !s.EndsWith(".config")).ToList();
+
+            if (!string.IsNullOrEmpty(txtFiltro.Text))
+            {
+                LerArquivos(filtro, arquivos);
+            }
+
+            HabilitarBotoes();
+        }
+
+        private void bntLimpar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lblResultado.Text);
+        }
+
+        private void LerArquivos(string[] filtro, List<string> arquivos)
+        {
+            foreach (var arquivo in arquivos)
+            {
+                try
+                {
+                    var stream = new FileStream(arquivo, FileMode.Open);
+
+                    using (StreamReader sr = new StreamReader(stream))
+                    {
+                        string linha = default;
+                        // Lê linha por linha
+                        while ((linha = sr.ReadLine()) != null)
+                        {
+                            foreach (var item in filtro)
+                            {
+                                if (linha.ToUpper().Contains(item.ToUpper()))
+                                {
+                                    lblResultado.Text += string.Format("{0} \n", arquivo);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    //Caso tenha algum arquivo sem acesso passa para o próximo
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Erro");
+                }
+            }
+
+            if (lblResultado.Text == string.Empty)
+            {
+                MessageBox.Show("Sem dados!", "Mensagem");
+            }
+        }
+        #endregion
+
+
+
+        #region |Métodos Privados|        
+
+        private void DesabilitarBotoes()
+        {
+            btnPesquisar.Enabled = false;
+            btnCopy.Enabled = false;
+            btnClean.Enabled = false;
+        }
+
+        private void HabilitarBotoes()
+        {
+            btnPesquisar.Enabled = true;
+            btnCopy.Enabled = true;
+            btnClean.Enabled = true;
+        }
+        #endregion
+
+    }
+}
